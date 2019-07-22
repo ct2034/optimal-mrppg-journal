e144d4a79bb656f7d736069c2b514548f4c8617f
Christian Henkel <post@henkelchristian.de>
2019-07-22 15:46:03 +0200

    two usability scriptz

---
+++ run.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+export GRB_LICENSE_FILE=/opt/gurobi811/linux64/gurobi.lic
+export GUROBI_HOME=/opt/gurobi811/linux64
+export LD_LIBRARY_PATH=/opt/gurobi811/linux64/lib/
+export PATH=/opt/gurobi811/linux64/bin/
+
+# from source ...
+/usr/lib/jvm/java-8-openjdk-amd64/bin/java \
+  -Djava.library.path=${LD_LIBRARY_PATH} \
+  -Dfile.encoding=UTF-8 \
+  -classpath \
+    ./bin:${LD_LIBRARY_PATH}/gurobi.jar \
+  projects.multipath.ILP.Main
