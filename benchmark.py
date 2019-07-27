1f7a82b19940da09291e6805cbc1a3e155720349
Christian Henkel <post@henkelchristian.de>
2019-07-27 22:07:16 +0200

    interesting setup

--- benchmark.py
+++ benchmark.py
@@ -10,8 +10,8 @@ import time
 GRAPH_FNAME = "graph.csv"
 GRAPH_UNDIR_FNAME = "graph_undir.csv"
 JOBS_FNAME = "tmp_jobs.csv"
-TIME_LIMIT = 6000
-SPLIT_LEVEL = 3
+TIME_LIMIT = 600
+SPLIT_LEVEL = 4
 
 
 def max_vertex():
@@ -116,10 +116,10 @@ if __name__ == '__main__':
     N = max_vertex()
     # ns = [1, 2, 3, 5, 10, 20, 30, 50, 100]
     # ns = range(1, 20)
-    ns = range(5, 100, 15)
+    ns = range(5, 200, 15)
     if not os.path.exists(GRAPH_UNDIR_FNAME):
         make_undir_graph_file(GRAPH_FNAME, GRAPH_UNDIR_FNAME)
-    for graph_fname in [GRAPH_FNAME, GRAPH_UNDIR_FNAME]:
+    for graph_fname in [GRAPH_UNDIR_FNAME, GRAPH_FNAME]:
         cs = []
         ts = []
         print("graph_fname: %s" % graph_fname)
@@ -127,5 +127,5 @@ if __name__ == '__main__':
             cost, t = plan_with_n_jobs(n_jobs, N, graph_fname)
             cs.append(cost)
             ts.append(t)
-            print("n_jobs: %3d | c: % 4.2f | t: % 3.2fs" % (n_jobs, cost, t))
+            print("n_jobs: %3d | c: % 7.2f | t: % 7.2fs" % (n_jobs, cost, t))
         assert len(cs) == len(ns), "all ns should have a cost"
